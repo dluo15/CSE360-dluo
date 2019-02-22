461f65b47633f144bc10b65d470165fad603e7ed
Darren <dluo15@asu.edu>
2019-02-22 16:43:17 -0700

    Initial version of Calculator.java

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -1,39 +1,71 @@
+/**
+ *Name: Darren Luo
+ *Class ID: 463
+ *CSE 360 Assignment #2
+ *Description: Calculator that performs addition, subtraction, multiplication,
+ *division, and obtains history of previous inputs into the calculator. 
+ */
+
 package cse360assign2;
 
 public class Calculator 
 {
 	private int total;
 	
+	/**
+	 * Constructor that creates a total variable to keep track of total in
+	 * calculator when performing functions
+	 */
 	public Calculator () 
 	{
 		total = 0;  // not needed - included for clarity
 	}
 	
+	/**
+	 * Returns total after mathematical operations are finished being 
+	 * performed on calculator
+	 */
 	public int getTotal () 
 	{
 		return 0;
 	}
 	
+	/**
+	 * Performs addition with values entered by the user
+	 */
 	public void add (int value) 
 	{
 		
 	}
 	
+	/**
+	 * Performs subtraction with values entered by the user
+	 */
 	public void subtract (int value) 
 	{
 		
 	}
 	
+	/**
+	 * Performs multiplication with values entered by the user
+	 */
 	public void multiply (int value) 
 	{
 		
 	}
 	
+	/**
+	 * Performs division with values entered by the user
+	 */
 	public void divide (int value) 
 	{
 		
 	}
 	
+	/**
+	 * Obtains history of mathematical operations performed on the 
+	 * calculator
+	 */
 	public String getHistory () 
 	{
 		return "";
