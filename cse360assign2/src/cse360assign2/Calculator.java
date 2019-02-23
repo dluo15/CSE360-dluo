ab1660f93105363ab02920974ae467cbef44025d
Darren <dluo15@asu.edu>
2019-02-22 19:05:36 -0700

    Second version of Calculator.java Modifies add, subtract, getTotal, multiply, and divide method. Also adds more javadoc comments.

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -24,47 +24,65 @@ public class Calculator
 	/**
 	 * Returns total after mathematical operations are finished being 
 	 * performed on calculator
+	 * 
+	 * @return current total which holds total after any mathematical
+	 * operations are performed
 	 */
 	public int getTotal () 
 	{
-		return 0;
+		return total;
 	}
 	
 	/**
 	 * Performs addition with values entered by the user
+	 * 
+	 * @param user-inputted value
 	 */
 	public void add (int value) 
 	{
-		
+		total = total + value;
 	}
 	
 	/**
 	 * Performs subtraction with values entered by the user
+	 * 
+	 * @param user-inputted value
 	 */
 	public void subtract (int value) 
 	{
-		
+		total = total - value;
 	}
 	
 	/**
 	 * Performs multiplication with values entered by the user
+	 * 
+	 * @param user-inputted value
 	 */
 	public void multiply (int value) 
 	{
-		
+		total = total * value;
 	}
 	
 	/**
 	 * Performs division with values entered by the user
+	 * 
+	 * @param user-inputted value
 	 */
 	public void divide (int value) 
-	{
+	{	
+		if (value == 0)
+		{
+			total = 0;
+		}
 		
+		total = total / value;
 	}
 	
 	/**
 	 * Obtains history of mathematical operations performed on the 
 	 * calculator
+	 * 
+	 * @return String containing history of mathematical operations performed
 	 */
 	public String getHistory () 
 	{
